1f3b3c1b9367f00cb7ae40cf118851febd7c794f
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2024-03-13 15:31:54 +0300

    Delete pro.py

--- pro.py
+++
@@ -1,8 +0,0 @@
-import os, platform
-os.system('xdg-open https://facebook.com/groups/3017062245271082/')
-os.system('git pull')
-bit = platform.architecture()[0]
-if bit == '64bit':
-    import kmk
-elif bit == '32bit':
-    import mkm
