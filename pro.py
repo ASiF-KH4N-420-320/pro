cb5ea5b5b978ae6534f10a1aefe9e1db614185a8
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2024-03-13 15:06:22 +0300

    Update pro.py

--- pro.py
+++ pro.py
@@ -3,6 +3,6 @@ os.system('xdg-open https://facebook.com/groups/3017062245271082/')
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
-    import asi1
+    import kmk
 elif bit == '32bit':
-    import asi1
+    import mkm
