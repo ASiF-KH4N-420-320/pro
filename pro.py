ca9344211d94e47c4be46eedfea44501ca7f3c8b
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2024-03-13 14:25:25 +0300

    Update pro.py

--- pro.py
+++ pro.py
@@ -1,1 +1,8 @@
-import asi1
+import os, platform
+os.system('xdg-open https://facebook.com/groups/3017062245271082/')
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import asi1
+elif bit == '32bit':
+    import asi1
